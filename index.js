c14fb68a94aefc844724880d75ecf5cf0d50eeb7
WillisLi <li.willis95@gmail.com>
2022-02-10 16:01:32 -0800

    Added guitars URI

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const server = require('/server');
+require("dotenv").config();
+
+const server = require('./server.js');
 
 const PORT = process.env.PORT || 7000;
 
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
+const mongoose = require("mongoose");
+const Guitars = require("./model");
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log("Connected.")
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/guitars', async (req, res) => {
-    res
+server.get('/guitars', (req, res) => {
+    Guitars.find({})
+        .then((guitars) => {
+            res.json(guitars);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 });
 
 module.exports = server; 
